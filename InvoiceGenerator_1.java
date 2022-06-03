f4bd41365d502b61e42b43c3f5afebda600e6e02
Rajudv19 <rajudv1997@gmail.com>
2022-06-03 23:16:33 +0530

    Added code for InvoiceGenerator1

---
+++ InvoiceGenerator_1.java
@@ -0,0 +1,10 @@
+package com.BridgeLabs.day30;
+
+public class InvoiceGenerator_1 {
+
+	public double CalculateFare(double distance, int time) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
