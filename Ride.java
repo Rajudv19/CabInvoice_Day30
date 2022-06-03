6605bc8e85d916a6c59a734e95db22fa31b37489
Rajudv19 <rajudv1997@gmail.com>
2022-06-03 23:18:30 +0530

    Added code for Ride

---
+++ Ride.java
@@ -0,0 +1,11 @@
+package com.BridgeLabs.day30;
+
+public class Ride {
+	public double distance;
+	public int time;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
