5cb0adb79c4b49b1e30f68b451cd72cafa849f4c
Rajudv19 <rajudv1997@gmail.com>
2022-06-03 23:17:26 +0530

    Added code for InvoiceGenerator2

---
+++ InvoiceGenerator_2.java
@@ -0,0 +1,20 @@
+package com.BridgeLabs.day30;
+
+public class InvoiceGenerator_2 {
+
+	public double CalculateFare(Ride[] rides) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double CalculateFare1(Ride[] rides) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double CalculateFare11(Ride[] rides) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
